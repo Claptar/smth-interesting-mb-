7c0638937d1c61e4eb8562f7126475d2030e5b50
Claptar <binkevich.as@phystech.edu>
2020-07-06 18:11:48 +0300

    Понты

--- Courses/Algo/1st/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Courses/Algo/1st/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,18 +1,21 @@
-# python3
-
-
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+    numbers.sort()
+    return numbers[len(numbers) - 1] * numbers[len(numbers) - 2]
 
-    return max_product
+
+def max_pairwise_product2(num):
+    maxi1 = 0
+    for i in range(1, len(num)):
+        if num[i] > num[maxi1]:
+            maxi1 = i
+    maxi2 = 0
+    for i in range(0, len(num)):
+        if num[i] > num[maxi2] and num[i] != num[maxi1]:
+            maxi2 = i
+    return num[maxi1] * num[maxi2]
 
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product2(input_numbers))
