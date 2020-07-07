fc7e6e6c5d4ff06b02f1fa602659727de3df7731
Claptar <binkevich.as@phystech.edu>
2020-07-07 15:48:46 +0300

    Конечная версия 2-го задания на классы

--- Courses/Python/1st/week 3/week03_02.py
+++ Courses/Python/1st/week 3/week03_02.py
@@ -28,10 +28,10 @@ class Truck(CarBase):
         super().__init__(photo_file_name, brand, carrying)
         self.car_type = 'truck'
         body_whl = body_whl.split('x')
-        if all(is_digit(x) for x in body_whl) and all(float(x) > 0 for x in body_whl):
+        if all(is_digit(x) for x in body_whl) and all(float(x) > 0 for x in body_whl) and len(body_whl) == 3:
             self.body_length, self.body_width, self.body_height = map(float, body_whl)
         else:
-            self.body_length, self.body_width, self.body_height = 0, 0, 0
+            self.body_length, self.body_width, self.body_height = 0.0, 0.0, 0.0
 
     def get_body_volume(self):
         return self.body_length * self.body_width * self.body_height
@@ -40,7 +40,7 @@ class Truck(CarBase):
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(photo_file_name, brand, carrying)
-        self.passenger_seats_count = passenger_seats_count
+        self.passenger_seats_count = int(passenger_seats_count)
         self.car_type = 'car'
 
 
@@ -57,24 +57,20 @@ def get_car_list(csv_filename):
         lines = f.readlines()
     for line in lines[1:]:
         line = line.strip().split(';')
-        if len(line) < 7 or os.path.splitext(line[3])[1] == '':
+        if len(line) < 7 or os.path.splitext(line[3])[1] not in ['.png', '.jpeg', '.jpg', '.gif'] or not is_digit(line[5]):
             continue
         elif line[0] == 'car':
             del line[6], line[4], line[0]
-            if '' not in line and all(is_digit(x) for x in [line[1], line[3]]):
-                car_list.append(Car(line[0], line[2], line[1], line[3]))
+            if '' not in line and line[1].isdigit():
+                car_list.append(Car(line[0], line[2], line[3], line[1]))
             else:
                 continue
         elif line[0] == 'truck':
             del line[6], line[2], line[0]
-            if '' not in line[:2] and is_digit(line[3]):
+            if '' not in line[:2]:
                 car_list.append(Truck(line[0], line[1], line[3], line[2]))
         elif line[0] == 'spec_machine':
             del line[4], line[2], line[0]
-            if all(x != '' for x in line[0:2] + [line[3]]) and is_digit(line[2]):
+            if '' not in line:
                 car_list.append(SpecMachine(line[0], line[1], line[2], line[3]))
     return car_list
-
-
-for x in get_car_list('coursera_week3_cars.csv'):
-    print(f'car_type = {x.car_type}, brand = {x.brand}')
