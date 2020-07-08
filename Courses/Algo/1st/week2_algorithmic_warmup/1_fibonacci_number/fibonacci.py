5653e472dcd80b7d2efd6695935c567ba092c00a
Claptar <binkevich.as@phystech.edu>
2020-07-08 23:14:10 +0300

    Фибоначи

--- Courses/Algo/1st/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Courses/Algo/1st/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,10 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    fib_num = [0, 1]
+    for i in range(1, n):
+        fib_num.append(fib_num[i] + fib_num[i - 1])
+    return fib_num[n]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
 
 n = int(input())
 print(calc_fib(n))
