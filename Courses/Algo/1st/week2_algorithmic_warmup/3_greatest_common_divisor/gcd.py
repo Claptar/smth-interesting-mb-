9bd073ae60993ffb1019e205d62026cfd68e5b34
Claptar <binkevich.as@phystech.edu>
2020-07-09 14:43:30 +0300

    Поиск НОД

--- Courses/Algo/1st/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Courses/Algo/1st/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,16 +1,22 @@
 # Uses python3
 import sys
 
-def gcd_naive(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
 
-    return current_gcd
+def gcd_naive(a, b):
+    if (a > b) and (b != 0):
+        return gcd_naive(b, a % b)
+    elif (a < b) and (b != 0):
+        return gcd_naive(a, b % a)
+    else:
+        return a
+
+
+def main():
+    inp = sys.stdin.read()
+    a, b = map(int, inp.split())
+    #a, b = map(int, input().split())
+    print(gcd_naive(a, b))
+
 
 if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    main()
