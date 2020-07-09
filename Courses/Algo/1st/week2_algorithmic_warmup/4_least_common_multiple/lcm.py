efb017db71f4f11fa9b6c976f129214d41928e03
Claptar <binkevich.as@phystech.edu>
2020-07-09 17:13:31 +0300

    Поиск НОК

--- Courses/Algo/1st/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Courses/Algo/1st/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,26 @@
 # Uses python3
 import sys
 
+
+def gcd_naive(a, b):
+    if (a > b) and (b != 0):
+        return gcd_naive(b, a % b)
+    elif (a < b) and (b != 0):
+        return gcd_naive(a, b % a)
+    else:
+        return a
+
+
 def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+    return a * b // gcd_naive(a, b)
 
-    return a*b
 
-if __name__ == '__main__':
+def main():
     input = sys.stdin.read()
     a, b = map(int, input.split())
     print(lcm_naive(a, b))
+
+
+if __name__ == '__main__':
+    main()
 
--- Courses/Algo/1st/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ Courses/Algo/1st/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,15 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
 
-    previous = 0
-    current  = 1
+def get_fibonacci_last_digit_naive(n, m):
+    fib_num = [0, 1]
+    for i in range(1, n):
+        fib_num.append((fib_num[i] + fib_num[i - 1]) % m)
+    return fib_num[n] % m
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.read()
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_last_digit_naive(n, m))
