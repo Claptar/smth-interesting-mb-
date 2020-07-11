d42467133230b0f9a3365234ee2020bd00ce5da7
Claptar <binkevich.as@phystech.edu>
2020-07-11 10:56:44 +0300

    Задача по выдаче сдачи

--- Courses/Algo/1st/week3_greedy_algorithms/1_money_change/change.py
+++ Courses/Algo/1st/week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,21 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m
+    money = [10, 5, 1]
+    count = 0
+    for coin in money:
+        num = m // coin
+        if m == 0:
+            return count
+        elif num != 0:
+            m -= num * coin
+            count += num
+    return count
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    #m = int(input())
     print(get_change(m))
