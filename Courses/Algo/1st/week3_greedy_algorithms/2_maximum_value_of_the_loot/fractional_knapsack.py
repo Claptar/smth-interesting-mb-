b78a227bd446b479f99dc6660d425dd803c5cc4c
Claptar <binkevich.as@phystech.edu>
2020-07-11 11:57:23 +0300

    Собираем рюкзак

--- Courses/Algo/1st/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Courses/Algo/1st/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,11 +1,21 @@
 # Uses python3
 import sys
 
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
-
-    return value
+    rho = []
+    for i in range(len(weights)):
+        rho.append(values[i] / weights[i])
+    for i in range(len(weights)):
+        if capacity == 0:
+            return value
+        else:
+            ind = rho.index(max(rho))
+            value += min(weights[ind], capacity) / weights[ind] * values[ind]
+            capacity -= min(weights[ind], capacity)
+            del values[ind], weights[ind], rho[ind]
+    return round(value, 4)
 
 
 if __name__ == "__main__":
@@ -15,3 +25,4 @@ if __name__ == "__main__":
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
+
