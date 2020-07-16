9aa2d88fa8c9baf9ad1b98191d7163bec27de713
Claptar <binkevich.as@phystech.edu>
2020-07-16 13:50:45 +0300

    4 неделя

--- Courses/Algo/1st/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ Courses/Algo/1st/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,22 +1,25 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
+def binary_search(a, low, high, x):
+    if low > high:
+        return -1
+    mid = low + (high - low) // 2
+    if a[mid] == x:
+        return mid
+    elif x < a[mid]:
+        return binary_search(a, low, mid - 1, x)
+    elif x > a[mid]:
+        return binary_search(a, mid + 1, high, x)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
+    a = data[1: n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, 0, len(a) - 1, x), end=' ')
--- Courses/Algo/1st/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ Courses/Algo/1st/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,46 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+
+def pivot(a, l, r):
+    k = a[l]
+    j = l
+    for i in range(l + 1, r + 1):
+        if a[i] <= k:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    return j
+
+
+def q_sort(a, l, r):
+    if l >= r:
+        return
+    m = pivot(a, l, r)
+    q_sort(a, l, m - 1)
+    q_sort(a, m + 1, r)
+
+
+def get_majority_element(a):
+    n = len(a)
+    q_sort(a, 0, len(a) - 1)
+    el = a[0]
+    count = 0
+    for i in range(n):
+        if count > n / 2:
+            return 1
+        elif a[i] == el:
+            count += 1
+        else:
+            el = a[i]
+            count = 0
+    if count >= n / 2:
+        return 1
+    else:
+        return 0
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    n = input()
+    a = list(map(int, input().split(' ')))
+    print(get_majority_element(a))
