4806e41dd858b3f5564c940b6d8b2b27e00a2a4c
Claptar <binkevich.as@phystech.edu>
2020-07-18 16:23:21 +0300

    Сдача

--- Courses/Algo/1st/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Courses/Algo/1st/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,19 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m // 4
+    coins = [1, 3, 4]
+    a = [0] * (m + 1)
+    for summa in range(1, m + 1):
+        min_coin_num = m
+        for coin in coins:
+            last_coin = summa - coin
+            if last_coin >= 0 and a[last_coin] + 1 < min_coin_num:
+                min_coin_num = a[summa - coin] + 1
+        a[summa] = min_coin_num
+    return a[m]
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
