40e5496af4a8110f371cc8cf7fb97d2d602c555e
Claptar <binkevich.as@phystech.edu>
2020-07-22 13:56:45 +0300

    Knapstack without repeats problem

--- Courses/Algo/1st/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ Courses/Algo/1st/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,19 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+
+def optimal_weight(W, rocks):
+    table = [[0] * (W + 1) for i in range(len(rocks) + 1)]
+    rocks = [0] + rocks
+    for i in range(1, len(rocks)):
+        for w in range(1, W + 1):
+            table[i][w] = table[i - 1][w]
+            if w >= rocks[i] and table[i - 1][w - rocks[i]] + rocks[i] > table[i][w]:
+                table[i][w] = table[i - 1][w - rocks[i]] + rocks[i]
+    return table[len(rocks) - 1][W]
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
+    W, n = map(int, input().split(' '))
+    w = list(map(int, input().split(' ')))
     print(optimal_weight(W, w))
