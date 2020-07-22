5f4ebac45254ca33e8c571d2607dc48bb3b266cd
Claptar <binkevich.as@phystech.edu>
2020-07-22 10:44:53 +0300

    Расстояние Левинштейна

--- Courses/Algo/1st/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ Courses/Algo/1st/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,26 @@
-# Uses python3
+import numpy as np
+
+
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    s = ' ' + s
+    t = ' ' + t
+    D = [[len(s) + len(t)] * len(s) for i in range(len(t))]
+    for j in range(len(s)):
+        D[0][j] = j
+    for i in range(len(t)):
+        D[i][0] = i
+    for i in range(1, len(t)):
+        for j in range(1, len(s)):
+            ins = D[i][j - 1] + 1
+            dell = D[i - 1][j] + 1
+            match = D[i - 1][j - 1]
+            mis_m = D[i - 1][j - 1] + 1
+            if s[j] == t[i]:
+                D[i][j] = min(ins, dell, match)
+            else:
+                D[i][j] = min(ins, dell, mis_m)
+    return D[len(t) - 1][len(s) - 1]
+
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
