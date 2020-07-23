7142149d3187aa9c887847257ace7072e58e971a
Claptar <binkevich.as@phystech.edu>
2020-07-23 23:50:38 +0300

    Графики распределения по областям

--- Courses/Algo/1st/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ Courses/Algo/1st/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -9,10 +9,37 @@ def evalt(a, b, op):
     else:
         assert False
 
-def get_maximum_value(dataset):
-    #write your code here
-    return 0
+
+def get_maximum_value(num, op):
+    n = len(num)
+    M = [[0] * n for i in range(n)]
+    m = [[0] * n for i in range(n)]
+    for i in range(n):
+        M[i][i] = num[i]
+        m[i][i] = num[i]
+    for i in range(1, n):
+        for j in range(n - i):
+            eq1 = evalt(M[j][i + j - 1], M[j + 1][i + j], op[i - 1 +j])
+            eq2 = evalt(m[j][i + j - 1], M[j + 1][i + j], op[i - 1 +j])
+            eq3 = evalt(M[j][i + j - 1], m[j + 1][i + j], op[i - 1 +j])
+            eq4 = evalt(m[j][i + j - 1], m[j + 1][i + j], op[i - 1 +j])
+            M[j][i + j] = max(eq1, eq2, eq3, eq4)
+            m[j][i + j] = min(eq1, eq2, eq3, eq4)
+    return M, m
+
+
+def dataset_conv(dataset):
+    num = []
+    op = []
+    for el in dataset:
+        if el.isdigit():
+            num.append(int(el))
+        else:
+            op.append(el)
+    return num, op
 
 
 if __name__ == "__main__":
-    print(get_maximum_value(input()))
+    dataset = '5-8+7*4-8+9'
+    num, op = dataset_conv(dataset)
+    print(get_maximum_value(num, op))
---
+++ Courses/Bioinf/1st course/week 4/RandomMotifSearch.py
@@ -0,0 +1,77 @@
+import random
+
+
+def profile_matrix(motifes_matrix):
+    n = len(motifes_matrix[0])
+    profile = {x:[0] * n for x in ['A', 'C', 'G', 'T']}
+    for i in range(n):
+        for motif in motifes_matrix:
+            profile[motif[i]][i] += 1 / n
+    return profile
+
+
+def Score(motifs):
+    score = 0
+    profile = profile_matrix(motifs)
+    for i in range(len(profile['A'])):
+        score += 1 - max([profile[x][i] for x in ['A', 'C', 'G', 'T']])
+    return score
+
+
+import random
+def random_motif_genetator(dna, k):
+    motifs = []
+    for string in dna:
+        i = random.randint(0, len(string) - k)
+        motifs.append(string[i:i + k])
+    return motifs
+
+
+def Profile_score(matrix, k_mer):
+    """
+    Using profile-matrix finds out score of k-mer
+                    A: 1/2 1/2 0 1/2
+    matrix          C: 1/4 1/4 0  0      k-mer: ACTG  
+    example:        G: 1/4  0  0 1/4
+                    T:  0  1/4 1 1/4
+    Output: 0.5 * 0.25 * 1 * 0.25 = 1/32
+    """
+    score = 1
+    for i in range(len(k_mer)):
+        score *= matrix[k_mer[i]][i]
+    return score
+
+
+def ProfileMostProbableKmer(text, k, profile):
+    """
+    Searching for the most probable k-mer in text
+    using profile-matrix
+    """
+    prob = 0
+    k_best = text[:k]
+    for i in range(len(text) - k + 1):
+        k_mer = text[i:i + k]
+        if Profile_score(profile, k_mer) > prob:
+            prob = Profile_score(profile, k_mer)
+            k_best = k_mer
+    return k_best
+
+
+def motifs_from_profile(dna, k, profile):
+    """
+    Using profile-matrix finding the most probable k-mer in each string
+    """
+    return [ProfileMostProbableKmer(string, k, profile) for string in dna]
+
+
+
+def randomized_motif_searsh(dna, k, t):
+    motifs = random_motif_genetator(dna, k)
+    best_motifs = motifs
+    while True:
+        profile = profile_matrix(motifs)
+        motifs = motifs_from_profile(dna, k, profile)
+        if Score(motifs) < Score(best_motifs):
+            best_motifs = motifs
+        else:
+            return best_motifs
